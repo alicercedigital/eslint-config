f918b30493bab2b5156b494188db72a8013c14f5
alicercedigital <contato@alicercedigital.com.br>
2024-08-16 15:37:33 -0300

    wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 const prettierRules = require("./prettier");
 
 module.exports = {
-  extends: [
-    // Pkgs:  eslint-config-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-n
-    "standard",
+ extends: [
+  // Pkgs:  eslint-config-standard eslint-plugin-promise eslint-plugin-import eslint-plugin-n
+  "standard",
 
-    // Pkgs: prettier eslint-plugin-prettier eslint-config-prettier
-    "plugin:prettier/recommended",
-  ],
-  rules: {
-    "prettier/prettier": ["error", prettierRules],
-  },
+  // Pkgs: prettier eslint-plugin-prettier eslint-config-prettier
+  "plugin:prettier/recommended",
+ ],
+ rules: {
+  "prettier/prettier": ["error", prettierRules],
+ },
 };
