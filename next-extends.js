3bb70577f5ad71f20c18fd588c815e2a45afb76a
dretz <contato@alicercedigital.com.br>
2024-09-04 13:51:14 -0300

    wip

---
+++ next-extends.js
@@ -0,0 +1,7 @@
+module.exports = [
+  // Pkgs: eslint-config-next
+  "next/core-web-vitals",
+
+  // Pkgs: prettier eslint-plugin-prettier eslint-config-prettier
+  "plugin:prettier/recommended",
+];
---
+++ next-plugins.js
@@ -0,0 +1,4 @@
+module.exports = [
+ // Pkgs: eslint-plugin-classnames
+ "classnames",
+];
